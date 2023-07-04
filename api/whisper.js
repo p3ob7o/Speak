54f84fb14aaa76bcac9fdf7c951cb0c155d4a3b0
Paolo Belcastro <p3ob7o@users.noreply.github.com>
2023-07-04 14:36:30 -0500

    guess?

--- api/whisper.js
+++ api/whisper.js
@@ -6,20 +6,21 @@ module.exports = async (req, res) => {
       method: 'POST',
       headers: {
         'Authorization': 'Bearer ' + process.env.OPENAI_API_KEY,
+        'Content-Type': 'application/json',
       },
-      body: {
+      body: JSON.stringify({
         file: req.body.audio_data,
         model: 'whisper-1',
         prompt: '', // Enter your prompt here if needed
         response_format: 'json', // Set the desired response format (json, text, srt, verbose_json, vtt)
         temperature: 0, // Set the desired temperature value (0 to 1)
         language: '', // Set the input audio language in ISO-639-1 format if known
-      },
+      }),
     });
 
     if (!response.ok) {
-      const errorText = await response.text(); // Get the error message from the response
-      console.error('Whisper API error:', errorText);
+      const errorResponse = await response.json(); // Get the error response
+      console.error('Whisper API error:', errorResponse.error.message);
       throw new Error('Whisper API request failed');
     }
 
