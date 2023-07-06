79bdcb55f58c505585cdf3e04c6cdac3a23b714c
Paolo Belcastro <p3ob7o@users.noreply.github.com>
2023-07-06 11:39:27 -0500

    Added error management

--- api/whisper.js
+++ api/whisper.js
@@ -24,30 +24,38 @@ const parseForm = req =>
     req.pipe(busboy);
   });
 
-module.exports = async (req, res) => {
-  if (req.method === 'POST') {
-    const file = await parseForm(req);
-    const formData = new FormData();
-
-    formData.append('audio', file, {
-      filename: 'audio.wav',
-      contentType: 'audio/wav',
-      knownLength: file.length
-    });
-
-    const whisperResponse = await axios.post(
-      'https://api.openai.com/v1/whisper/recognize',
-      formData,
-      {
-        headers: {
-          ...formData.getHeaders(),
-          Authorization: `Bearer ${process.env.OPENAI_KEY}`
-        }
+  module.exports = async (req, res) => {
+    try {
+      if (req.method === 'POST') {
+        const file = await parseForm(req);
+        console.log(`File Length: ${file.length}`);
+        const formData = new FormData();
+  
+        formData.append('audio', file, {
+          filename: 'audio.wav',
+          contentType: 'audio/wav',
+          knownLength: file.length
+        });
+  
+        console.log('Form Data:', formData);
+  
+        const whisperResponse = await axios.post(
+          'https://api.openai.com/v1/whisper/recognize',
+          formData,
+          {
+            headers: {
+              ...formData.getHeaders(),
+              Authorization: `Bearer ${process.env.OPENAI_KEY}`
+            }
+          }
+        );
+  
+        res.status(200).json(whisperResponse.data);
+      } else {
+        res.status(405).send('Method not allowed');
       }
-    );
-
-    res.status(200).json(whisperResponse.data);
-  } else {
-    res.status(405).send('Method not allowed');
-  }
-};
+    } catch (error) {
+      console.error('An error occurred:', error);
+      res.status(500).json({ message: 'An error occurred', error: error.message });
+    }
+  };  
